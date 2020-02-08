5e6e5edf60dce12c28d648b9ecea179126d4fec9
Kayla Dailey <kayladailey000@gmail.com>
2020-02-07 22:51:30 -0600

    Completed SMurf form

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { SmurfContext } from '../contexts/SmurfContext';
 import axios from 'axios';
 import "./App.css";
+import SmurfForm from "./SmurfForm";
 
 
 
@@ -12,7 +13,7 @@ const  App =  () =>{
       axios
         .get("http://localhost:3333/smurfs")
         .then(res => setSmurf(res.data))
-       // .then(res => console.log(res))
+        //.then(res => console.log(res))
         .catch(err => console.log(err));
     }, []);
 
@@ -22,6 +23,8 @@ const  App =  () =>{
       <SmurfContext.Provider value={smurf}>
       <div className="container">
         <h1>Smurf Village</h1>
+        <SmurfForm />
+  <h2>{}</h2>
       </div>
   </SmurfContext.Provider>
     );
---
+++ smurfs/src/components/SmurfForm.js
@@ -0,0 +1,62 @@
+import React, {  useState } from 'react';
+
+const SmurfForm = ({ addSmurf, error }) => {
+   const [name, setName] = useState('');
+   const [age, setAge] = useState('');
+   const [height, setHeight] = useState('');
+
+   const onSubmit = e => {
+      e.preventDefault();
+      addSmurf({
+         name,
+         age,
+         height
+      });
+      setName('');
+      setAge('');
+      setHeight('');
+   };
+
+   return (
+      <div>
+
+       
+
+         <form onSubmit={onSubmit} className="form">
+
+         <h2>Add A Smurf:</h2>
+         
+            <input
+            placeholder="Name"
+            className="input"
+            type="text"
+            value={name}
+            onChange={e => setName(e.target.value)} />
+
+       
+            <input
+             placeholder="Age"
+            className="input"
+            type="text"
+            value={age}
+            onChange={e => setAge(e.target.value)} />
+
+
+            <input
+             placeholder="Height"
+            className="input"
+            type="text"
+            value={height}
+            onChange={e => setHeight(e.target.value)} />
+
+<br />
+         
+      <button type="submit">Add Smurf</button>
+
+
+            </form>
+         </div>
+   )
+}
+
+export default SmurfForm;
