8bca024019f864f08047e42093305074a71fb9bf
Kayla Dailey <kayladailey000@gmail.com>
2020-02-07 20:36:52 -0600

    Setup Context

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { SmurfContext } from '../contexts/SmurfContext';
 import "./App.css";
 
 
@@ -6,12 +7,11 @@ import "./App.css";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+      <SmurfContext.Provider value={smurf}>
+      <div className="container">
+          <User />
       </div>
+  </SmurfContext.Provider>
     );
   }
 }
