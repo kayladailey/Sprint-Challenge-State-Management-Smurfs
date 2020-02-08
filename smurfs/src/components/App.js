f2d389fa924845cfdf47a9d62410cff14338ab35
Kayla Dailey <kayladailey000@gmail.com>
2020-02-07 21:44:33 -0600

    receiving data from API

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,19 +1,30 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { SmurfContext } from '../contexts/SmurfContext';
+import axios from 'axios';
 import "./App.css";
 
 
 
-class App extends Component {
-  render() {
-    return (
+const  App =  () =>{
+  const [smurf, setSmurf] = useState({ });
+
+    useEffect(() => {
+      axios
+        .get("http://localhost:3333/smurfs")
+        .then(res => setSmurf(res.data))
+       // .then(res => console.log(res))
+        .catch(err => console.log(err));
+    }, []);
+
+
+
+  return (
       <SmurfContext.Provider value={smurf}>
       <div className="container">
-          <User />
+        <h1>Smurf Village</h1>
       </div>
   </SmurfContext.Provider>
     );
   }
-}
 
 export default App;
